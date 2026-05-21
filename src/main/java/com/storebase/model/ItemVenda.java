1a90a9d485e8497472894cb69f2453c1ca7eaf5b
Marinoleo96351 <marinoleo51@gmail.com>
2026-05-21 19:56:12 -0300

    Implementação do Itemvenda

---
+++ src/main/java/com/storebase/model/ItemVenda.java
@@ -0,0 +1,35 @@
+package com.storebase.model;
+
+public class ItemVenda {
+
+    private Produto produto;
+    private int quantidade;
+    private double subtotal;
+
+    public ItemVenda() {}
+
+    public ItemVenda(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.subtotal = produto.getPrecoVenda() * quantidade;
+    }
+
+    public void calcularSubtotal() {
+        this.subtotal = produto.getPrecoVenda() * quantidade;
+    }
+
+    public Produto getProduto() { return produto; }
+    public void setProduto(Produto produto) { this.produto = produto; }
+
+    public int getQuantidade() { return quantidade; }
+    public void setQuantidade(int quantidade) { this.quantidade = quantidade; }
+
+    public double getSubtotal() { return subtotal; }
+    public void setSubtotal(double subtotal) { this.subtotal = subtotal; }
+
+    @Override
+    public String toString() {
+        return "ItemVenda{produto=" + produto.getNome() + ", quantidade=" + quantidade
+                + ", subtotal=" + subtotal + "}";
+    }
+}
