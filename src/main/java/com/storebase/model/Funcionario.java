c4b86bc0d167edba15b5fa8b0dee034a66e7f429
Marinoleo96351 <marinoleo51@gmail.com>
2026-05-21 19:20:37 -0300

    Implementação da funcionario.java

---
+++ src/main/java/com/storebase/model/Funcionario.java
@@ -0,0 +1,45 @@
+package com.storebase.model;
+
+public class Funcionario {
+
+    private int id;
+    private String nome;
+    private String cargo;
+    private String login;
+    private String senha;
+    private double salario;
+
+    public Funcionario() {}
+
+    public Funcionario(String nome, String cargo, String login, String senha, double salario) {
+        this.nome = nome;
+        this.cargo = cargo;
+        this.login = login;
+        this.senha = senha;
+        this.salario = salario;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getCargo() { return cargo; }
+    public void setCargo(String cargo) { this.cargo = cargo; }
+
+    public String getLogin() { return login; }
+    public void setLogin(String login) { this.login = login; }
+
+    public String getSenha() { return senha; }
+    public void setSenha(String senha) { this.senha = senha; }
+
+    public double getSalario() { return salario; }
+    public void setSalario(double salario) { this.salario = salario; }
+
+    @Override
+    public String toString() {
+        return "Funcionario{id=" + id + ", nome='" + nome + "', cargo='" + cargo
+                + "', login='" + login + "'}";
+    }
+}
