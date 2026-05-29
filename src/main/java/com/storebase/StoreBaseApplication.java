c29eddf051fd6227f5489c58acaef58d2bfd0794
leonardo <marinoleo51@gmail.com>
2026-05-29 19:51:20 -0300

    Implementação do Storebase.Aplication

---
+++ src/main/java/com/storebase/StoreBaseApplication.java
@@ -0,0 +1,11 @@
+package com.storebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreBaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StoreBaseApplication.class, args);
+    }
+}
